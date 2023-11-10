e18c5cf2e21a4d76001089be4cd85623f854fb7f
garrrikkotua <kotuaigor@gmail.com>
2023-11-10 16:53:47 +0300

    update cta

--- src/components/website/cta.component.tsx
+++ src/components/website/cta.component.tsx
@@ -1,6 +1,80 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { CHECKOUT_URL_STARTER } from "@/lib/constants";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+const Ratings = () => {
+  return (
+    <div className="flex flex-col lg:flex-row gap-2 items-center justify-center space-x-4">
+      <div className="flex overflow-hidden">
+        <Avatar>
+          <AvatarImage src="/avt1.jpeg" />
+          <AvatarFallback>LP</AvatarFallback>
+        </Avatar>
+        <Avatar>
+          <AvatarImage src="/avt2.jpeg" />
+          <AvatarFallback>LP</AvatarFallback>
+        </Avatar>
+        <Avatar>
+          <AvatarImage src="/avt3.jpeg" />
+          <AvatarFallback>LP</AvatarFallback>
+        </Avatar>
+        <Avatar>
+          <AvatarImage src="/avt4.jpeg" />
+          <AvatarFallback>LP</AvatarFallback>
+        </Avatar>
+        <Avatar>
+          <AvatarImage src="/avt5.jpeg" />
+          <AvatarFallback>LP</AvatarFallback>
+        </Avatar>
+      </div>
+      <p className="mx-auto max-w-lg text-sm text-gray-500 lg:mx-0">
+        <strong>1,000+</strong> AI pictures already created
+      </p>
+      <div className="flex">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-4 h-4 text-yellow-400"
+        >
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-4 h-4 text-yellow-400"
+        >
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-4 h-4 text-yellow-400"
+        >
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-4 h-4 text-yellow-400"
+        >
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-4 h-4 text-yellow-400"
+        >
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
 
 export const CTA = () => {
   return (
@@ -81,8 +155,11 @@ export const CTA = () => {
               asChild
               className="inline-flex items-center px-6 py-6 mt-8 font-semibold text-white bg-black transition-all duration-200 rounded-lg lg:mt-16"
             >
-              <Link href={CHECKOUT_URL_STARTER}>Create Logo Art Now →</Link>
+              <Link href="/#pricing">Create Logo Art Now →</Link>
             </Button>
+            <div className="mt-16">
+              <Ratings />
+            </div>
           </div>
         </div>
       </div>
