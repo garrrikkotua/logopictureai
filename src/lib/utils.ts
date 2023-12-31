18830d896534f84771eb288a68690877d0fb4cc1
garrrikkotua <kotuaigor@gmail.com>
2024-01-01 00:53:09 +0300

    remove cdn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function cdn(path: string) {
-  if (process.env.NODE_ENV === 'development') {
-    return path;
-  }
-  return `${process.env.NEXT_PUBLIC_CDN_URL}${path}`
+  return path;
+  // if (process.env.NODE_ENV === 'development') {
+  //   return path;
+  // }
+  // return `${process.env.NEXT_PUBLIC_CDN_URL}${path}`
 }
